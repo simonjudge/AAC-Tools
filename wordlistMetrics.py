215665fa28365a8631c51eaf313e2c5a5c096bf7
unknown <judges@lt-3655.bdgh-tr.trent.nhs.uk>
2012-01-29 21:08:47 +0000

    Added output of simple metrics to file...
    TODO: prepare for multiple file inputs (lots of wordlists from different folders)

--- wordlistMetrics.py
+++ wordlistMetrics.py
@@ -28,10 +28,13 @@ def parse_wordlist(wordlistxml='wordlist.xml', outputpath='.'):
     '''
     Parse wordlist.xml file. Just do the big one for now
     '''
-    
+
+    raw_words_out = open(outputpath + 'raw-words.text', 'wb')
+    raw_phrases_out = open(outputpath + 'raw-phrases.txt', 'wb')
     ldata_out = UnicodeWriter(open(outputpath + 'linguistic-data.csv', 'wb'), delimiter=',', quotechar='"')
     lsummary_out = UnicodeWriter(open(outputpath + 'linguistic-metrics.csv', 'wb'), delimiter=',', quotechar='"')
-    ldata_out.writerow(["WORD", "COUNT", "TYPE"])
+    ldata_out.writerow(["WORD", "NUMBER OF WORDS", "COUNT", "TYPE"])
+    lsummary_out.writerow(["File Name", "Total Words or Phrases", "Total Unique Words or Phrases", "Total Words", "Total Phrases"])
     words = []
     phrases = []
 
@@ -47,12 +50,14 @@ def parse_wordlist(wordlistxml='wordlist.xml', outputpath='.'):
        item_count = allwords.count(item)
        if num_words == 1:                          # Single word
           word_type = nltk.pos_tag(item)[-1][-1]
-          ldata_out.writerow([item, str(item_count), word_type, str(num_words)])
+          ldata_out.writerow([item, str(num_words), str(item_count), word_type])
           words.append(item)
        elif num_words > 1:                         # Phrase
           nltk_words = nltk.word_tokenize(item)
-          word_types = nltk.pos_tag(nltk_words) ### HOW TO DEAL WITH PHRASES???
-          ldata_out.writerow([item, str(item_count), str(word_types), str(num_words)])
+          word_pos = nltk.pos_tag(nltk_words) ### HOW TO DEAL WITH PHRASES???
+          word_types = [x[1] for x in word_pos]
+          ldata_out.writerow([item, str(num_words), str(item_count), " ,".join(word_types)])
+# HOW TO OUTPUT EACH POS TO A COLUMN???
           phrases.append(item)
 
     total_wordsphrases = len(allwords)
@@ -60,13 +65,13 @@ def parse_wordlist(wordlistxml='wordlist.xml', outputpath='.'):
     total_words = len(words)
     total_phrases = len(phrases)
 
-    print total_wordsphrases
-    print total_uwordsphrases
-    print total_words
-    print total_phrases
-    print words
-    print phrases
+# HOW TO MAKE THIS WORK WITH MULTIPLE WORDLISTS? E.G, WALKING THROUGH FOLDERS... ADD ROW PER WORDLIST.
+# MAKE SURE THAT WORD LISTS ARE NAMED AS THE GRID (FOR SINGLE FILES).
+    lsummary_out.writerow([os.path.split(wordlistxml)[-1], str(total_wordsphrases), str(total_uwordsphrases), str(total_words), str(total_phrases)])
 
+    raw_words_out.writelines('\n'.join(words))
+    raw_phrases_out.writelines('\n'.join(phrases))
+    
 def usage():
     print """
     This program takes a Grid 2 wordlist file
