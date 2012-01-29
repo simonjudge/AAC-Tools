2049a982a172ef34ff8a3ef5eec0f2ad484e3ebb
unknown <judges@lt-3655.bdgh-tr.trent.nhs.uk>
2012-01-29 19:43:49 +0000

    Implemented some basic metrics by adding to the itteration of all 'words'.
    TODO: deal with phrases properly.

--- wordlistMetrics.py
+++ wordlistMetrics.py
@@ -28,8 +28,12 @@ def parse_wordlist(wordlistxml='wordlist.xml', outputpath='.'):
     '''
     Parse wordlist.xml file. Just do the big one for now
     '''
-    linguistics_out = UnicodeWriter(open(outputpath + 'linguistic-metrics.csv', 'wb'), delimiter=',', quotechar='"')
-    linguistics_out.writerow(["WORD", "COUNT", "TYPE"])
+    
+    ldata_out = UnicodeWriter(open(outputpath + 'linguistic-data.csv', 'wb'), delimiter=',', quotechar='"')
+    lsummary_out = UnicodeWriter(open(outputpath + 'linguistic-metrics.csv', 'wb'), delimiter=',', quotechar='"')
+    ldata_out.writerow(["WORD", "COUNT", "TYPE"])
+    words = []
+    phrases = []
 
     tree = etree.parse(wordlistxml)
     #Ok many ways to do this
@@ -37,11 +41,31 @@ def parse_wordlist(wordlistxml='wordlist.xml', outputpath='.'):
     
     # how many times is a word/phrase mentioned?     
     uniqueWords = uniqueSet(allwords)              # Set of unique words.
-    pdb.set_trace()
    # Output metrics to file.
     for item in uniqueWords:
-       linguistics_out.writerow([item, str(allwords.count(item)), nltk.pos_tag(item)[-1][-1]])
-   
+       num_words = len(item.split())
+       item_count = allwords.count(item)
+       if num_words == 1:                          # Single word
+          word_type = nltk.pos_tag(item)[-1][-1]
+          ldata_out.writerow([item, str(item_count), word_type, str(num_words)])
+          words.append(item)
+       elif num_words > 1:                         # Phrase
+          nltk_words = nltk.word_tokenize(item)
+          word_types = nltk.pos_tag(nltk_words) ### HOW TO DEAL WITH PHRASES???
+          ldata_out.writerow([item, str(item_count), str(word_types), str(num_words)])
+          phrases.append(item)
+
+    total_wordsphrases = len(allwords)
+    total_uwordsphrases = len(uniqueWords)
+    total_words = len(words)
+    total_phrases = len(phrases)
+
+    print total_wordsphrases
+    print total_uwordsphrases
+    print total_words
+    print total_phrases
+    print words
+    print phrases
 
 def usage():
     print """
